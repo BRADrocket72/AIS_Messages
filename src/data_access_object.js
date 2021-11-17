21a48f470a8145616f4f3f44db7366cab1fc3157
BRADrocket72 <bvaal@ymail.com>
2021-11-17 09:50:59 -0500

    created one call, to check db for data

--- src/data_access_object.js
+++ src/data_access_object.js
@@ -1,5 +1,19 @@
 const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017';
 const dbName = 'AISTestData';
+(async function(){
+    var client = new MongoClient(url, {useNewUrlParser: true});
+    try {
+        await client.connect()
+        const vessels = client.db(dbName).collection('vessels');
+        var docs =await vessels.findOne();
+        console.log(docs);
+    } catch (error) {
+        console.log(error)
+    }finally{
+        client.close()
+    }
+    
+})()
 
 
