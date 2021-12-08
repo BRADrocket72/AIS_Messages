78bd5887d6771cfcbe6a4228425a801db2e22090
Janardhen Jayavel <jjanardhen@Janardhens-MacBook-Air.local>
2021-12-08 17:40:41 -0500

    get data and print

--- src/data-access-object.js
+++ src/data-access-object.js
@@ -23,3 +23,44 @@ modules.exports = {
 }
 
 
+
+
+
+
+//Function to get all data and print it 
+
+// create client so we connec to database
+const client = new MongoClient(url);
+
+// run function is used to connect with collection of database to read the data from database
+async function run() {
+    try {
+        // connect with databaee
+        await client.connect();
+        // print the message that we successfully connected with database
+        console.log("Connected correctly to server");
+      // load the countryDatabase and store in db variable
+        const db = client.db(dbName)
+    // load the collection named as MyData and store in col variable
+        const col= db.collection("vessels")
+ // use find function to get all the data, then print the data to on console     
+        col.find({}).toArray().then((ans)=>{console.log(ans)}).catch();
+// find one function used to get only one document or data and print it on console
+        const data= await col.findOne()
+        console.log(data)
+
+    } 
+      // if we catch error then print the error on console
+    catch (err) {
+        console.log(err.stack);
+    }
+   // at last, we nned to close the client
+    finally {
+        await client.close();
+    }
+}
+// call the run function
+run().catch(console.dir);
+
+
+
